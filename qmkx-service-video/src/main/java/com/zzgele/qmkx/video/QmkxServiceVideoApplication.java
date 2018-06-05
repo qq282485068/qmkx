110505ccb54ac24df5cf429a3beaf16e6c3ca37f
haijun.liu <2020068376@qq.com>
2018-06-05 10:03:29 +0800

    add video module

---
+++ qmkx-service-video/src/main/java/com/zzgele/qmkx/video/QmkxServiceVideoApplication.java
@@ -0,0 +1,12 @@
+package com.zzgele.qmkx.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmkxServiceVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QmkxServiceVideoApplication.class, args);
+    }
+}
---
+++ qmkx-service-video/src/test/java/com/zzgele/qmkx/video/QmkxServiceVideoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzgele.qmkx.video;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QmkxServiceVideoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
